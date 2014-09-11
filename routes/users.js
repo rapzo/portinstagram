75df923d948047760fb11ab1b2aeba5c4f4f009c
ruipedro.lima@gmail.com <ruipedro.lima@gmail.com>
2014-09-11 15:28:22 +0100

    Updated views & routes related to userland.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var User = require('models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  User.fetchAll().then(function (users) {
+    res.render('users/index', { users: users.toJSON() });
+  });
+});
+
+router.get('/:id', function (req, res) {
+  new User({ id: req.param('id') }).fetch().then(function (user) {
+    res.render('users/view', { user: user.toJSON() });
+  });
 });
 
 module.exports = router;
